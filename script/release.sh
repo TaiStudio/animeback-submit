d121af26f53eaa558094a0ab092b32ad199ca7cd
LeGitHubDeTai <tai.studio@outlook.fr>
2021-05-07 18:42:54 +0200

    add and update

---
+++ script/release.sh
@@ -0,0 +1,28 @@
+set -v
+set -o errexit
+set -o pipefail
+set -o nounset
+
+git clone "https://github.com/TaiStudio/animeback-submit" app
+cd app
+npm ci
+
+npm run test-all
+
+if [ "$(git status --porcelain)" = "" ]; then
+  echo "no new content found; goodbye!"
+  exit
+fi
+
+git config user.email tai.studio@outlook.fr
+git config user.name animebot
+git add .
+git commit -am "update extensions" --author "AnimeBot <tai.studio@outlook.fr>"
+npm version minor -m "bump minor to %s"
+git pull --rebase
+git push origin master
+git push origin master --tags
+
+npm publish
+npm pack
+node ./script/publish-to-gh.js
