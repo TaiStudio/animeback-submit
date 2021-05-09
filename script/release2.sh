2cd456e951c703cd0a15aee3008c6f6dd2dde140
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2021-05-09 21:52:34 +0200

    update and add

---
+++ script/release2.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -v            # print commands before execution, but don't expand env vars in output
+set -o errexit    # always exit on error
+set -o pipefail   # honor exit codes when piping
+set -o nounset    # fail on unset variables
+
+git clone "https://taistudio:$GH_TOKEN@github.com/TaiStudio/animeback-submit" extension
+cd extension
+npm ci
+
+npm run test-all
+
+# bail if nothing changed
+if [ "$(git status --porcelain)" = "" ]; then
+  echo "no new content found; goodbye!"
+  exit
+fi
+
+git config user.email tai.studio@outlook.fr
+git config user.name taistudio
+git add .
+git commit -am "update extensions" --author "Tai Studio <tai.studio@outlook.fr>"
+npm version minor -m "bump minor to %s"
+git pull --rebase
+git push origin master
+git push origin master --tags
+echo @taistudio:registry=https://npm.pkg.github.com > .npmrc
+npm publish
+npm pack
+node ./script/publish-to-gh.js
