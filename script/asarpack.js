dcb2f6ca4b2ff0eb709d9aaffe01de3f9aab41be
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2021-05-10 17:46:17 +0200

    Update asarpack.js

--- script/asarpack.js
+++ script/asarpack.js
@@ -2,18 +2,16 @@ const fs = require("fs");
 const path = require("path");
 const asar = require("asar");
 const extensions = require("../lib/raw-extensions-list")();
-const rimraf = require("rimraf").sync;
 
 console.log("Create extension submission asar file...");
 
 extensions.forEach((extension) => {
   if (!fs.existsSync(extension.asarPath)) {
-    var src = `extensions/${extension.slug}/${extension.slug}/files`;
+    var src = path.join(__dirname, `../extensions/${extension.slug}/files`);
     var dest = `${extension.asarPath}`;
 
     asar.createPackage(src, dest);
 
-    rimraf(path.join(__dirname, src));
     console.log("done.");
   }
 });
