7586d91ee0c4d8ce1ba800ea7edb4b34f11417cd
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2021-05-10 17:46:19 +0200

    Add files via upload

---
+++ script/asarclean.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const path = require("path");
+const extensions = require("../lib/raw-extensions-list")();
+const rimraf = require("rimraf").sync;
+
+console.log("Clean extension submission files dir...");
+
+extensions.forEach((extension) => {
+  var src = path.join(__dirname, `../extensions/${extension.slug}/files`);
+  if (!fs.existsSync(src)) {
+
+    rimraf(src);
+
+    console.log("done.");
+  }
+});
