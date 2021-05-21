5c38abf3b1a856c95c7e07afe580620235dea869
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2021-05-21 16:27:33 +0200

    feat: add spotify

---
+++ extensions/spotify/files/main.js
@@ -0,0 +1,157 @@
+const a0_0x764d = [
+  "express",
+  "request",
+  "454327fqlRhY",
+  "377563BLdyAX",
+  "images",
+  "loop",
+  "substring",
+  "currently_playing_type",
+  "1nlLmcY",
+  "getURL",
+  "\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<script>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20$(\x27.spotify\x20img\x27).attr(\x27src\x27,\x20\x27",
+  "https://api.spotify.com/v1/me/player/currently-playing?market=FR",
+  "&redirect_uri=http://localhost/call/&scope=user-read-currently-playing\x20user-read-playback-state&response_type=token",
+  "exports",
+  "did-frame-finish-load",
+  "artists",
+  "name",
+  "replace",
+  "\x22);\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20$(\x27.spotify\x20.name\x27).text(\x22",
+  "346342YkAPNQ",
+  "log",
+  "192667HkzUCe",
+  "item",
+  "album",
+  "1juhNMk",
+  "acbc7b77f99a43008283911a76f287b8",
+  "webContents",
+  "61636rJdWCJ",
+  "\x22);\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</script>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20",
+  "\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<style>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20.spotify{\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20position:\x20absolute;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20top:\x2030%;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20right:\x200.5%;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20z-index:999;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20width:\x2015%;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20height:\x2035%;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20border:\x20white\x202px\x20solid;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20border-radius:\x2010px;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20overflow:\x20hidden;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20text-align:\x20center;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20background:\x20#1ed760;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20}\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20.spotify\x20img{\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20position:\x20relative;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20height:\x20auto;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20}\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20.spotify\x20.infos{\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20display:\x20block;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20}\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20.spotify\x20.infos\x20p{\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20margin:\x200;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20}\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</style>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22spotify\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<img\x20src=\x22https://www.scdn.co/i/_global/twitter_card-default.jpg\x22></img>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22infos\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<p\x20class=\x22artists\x22>Artist\x20Name</p>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<p\x20class=\x22album\x22>Album\x20Name</p>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<p\x20class=\x22name\x22>Music\x20Name</p>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20",
+  "application/json",
+  "listen",
+  "31Whyqoe",
+  "access_token",
+  "7135oRcEjZ",
+  "https://accounts.spotify.com/authorize?client_id=",
+  "statusCode",
+  "send",
+  "ExtensionKit",
+  "495498YBSmLG",
+  "get",
+  "4rQSnvr",
+];
+const a0_0x572145 = a0_0x4c5b;
+function a0_0x4c5b(_0x2940ce, _0xe6a869) {
+  _0x2940ce = _0x2940ce - 0x192;
+  let _0x764d6f = a0_0x764d[_0x2940ce];
+  return _0x764d6f;
+}
+(function (_0x3a6cf0, _0x4eb5e3) {
+  const _0x3c7c83 = a0_0x4c5b;
+  while (!![]) {
+    try {
+      const _0x11e133 =
+        parseInt(_0x3c7c83(0x1b2)) +
+        parseInt(_0x3c7c83(0x19d)) * parseInt(_0x3c7c83(0x1ac)) +
+        parseInt(_0x3c7c83(0x1a0)) * parseInt(_0x3c7c83(0x1ae)) +
+        -parseInt(_0x3c7c83(0x1b1)) +
+        -parseInt(_0x3c7c83(0x1a5)) * parseInt(_0x3c7c83(0x1a7)) +
+        -parseInt(_0x3c7c83(0x198)) +
+        parseInt(_0x3c7c83(0x19a)) * parseInt(_0x3c7c83(0x1b7));
+      if (_0x11e133 === _0x4eb5e3) break;
+      else _0x3a6cf0["push"](_0x3a6cf0["shift"]());
+    } catch (_0x22b825) {
+      _0x3a6cf0["push"](_0x3a6cf0["shift"]());
+    }
+  }
+})(a0_0x764d, 0x46e72);
+const extensionConfig = require("./extension.json"),
+  request = require(a0_0x572145(0x1b0)),
+  express = require(a0_0x572145(0x1af)),
+  app = express(),
+  port = 0x50;
+var spotify_clienID = a0_0x572145(0x19e),
+  win = null,
+  spotify_code = null;
+app[a0_0x572145(0x1ad)]("/call", (_0x503adb, _0x2f4cb7) => {
+  const _0x21b83c = a0_0x572145;
+  (spotify_code = _0x503adb["query"][_0x21b83c(0x1a6)]),
+    _0x2f4cb7[_0x21b83c(0x1aa)](win["webContents"]["getURL"]()),
+    win[_0x21b83c(0x19f)]["on"](_0x21b83c(0x193), function () {
+      const _0x1f3f1e = _0x21b83c;
+      var _0xf1b4a8 = win[_0x1f3f1e(0x19f)][_0x1f3f1e(0x1b8)](),
+        _0xdff1f6 = _0xf1b4a8[_0x1f3f1e(0x1b5)](
+          _0xf1b4a8["indexOf"]("#") + 0x1
+        );
+      (spotify_code = _0xdff1f6[_0x1f3f1e(0x196)]("access_token=", "")),
+        win["destroy"](),
+        (win = null);
+    });
+}),
+  app[a0_0x572145(0x1a4)](port, () => {
+    const _0x1e2959 = a0_0x572145;
+    console[_0x1e2959(0x199)]("listening\x20at\x20http://localhost:" + port);
+  }),
+  (module[a0_0x572145(0x192)] = {
+    main(_0x51f15b) {
+      const _0xb21d4f = a0_0x572145;
+      (win = _0x51f15b["ExtensionKit"]["createWindow"](
+        null,
+        _0xb21d4f(0x1a8) + spotify_clienID + _0xb21d4f(0x1bb),
+        0x28a,
+        0x30c,
+        !![],
+        !![],
+        !![],
+        ![]
+      )),
+        _0x51f15b[_0xb21d4f(0x1ab)]["wallpaperExtend"](
+          _0xb21d4f(0x1a2),
+          extensionConfig[_0xb21d4f(0x195)]
+        ),
+        this[_0xb21d4f(0x1b4)](_0x51f15b),
+        setInterval(() => {
+          const _0x3f4b60 = _0xb21d4f;
+          this[_0x3f4b60(0x1b4)](_0x51f15b);
+        }, 0x1388);
+    },
+    loop(_0x4e4835) {
+      const _0x46fa31 = a0_0x572145,
+        _0x287bef = {
+          url: _0x46fa31(0x1ba),
+          headers: {
+            "User-Agent": _0x46fa31(0x1b0),
+            Accept: _0x46fa31(0x1a3),
+            "Content-Type": "application/json",
+            Authorization: "Bearer\x20" + spotify_code,
+          },
+        };
+      function _0x4fb107(_0x7417e5, _0x3f03ec, _0x43d6ba) {
+        const _0x20b784 = _0x46fa31;
+        if (!_0x7417e5 && _0x3f03ec[_0x20b784(0x1a9)] == 0xc8) {
+          const _0x517d9a = JSON["parse"](_0x43d6ba);
+          _0x517d9a[_0x20b784(0x1b6)] != "ad" &&
+            _0x517d9a != null &&
+            _0x4e4835[_0x20b784(0x1ab)]["wallpaperExtend"](
+              _0x20b784(0x1b9) +
+                _0x517d9a[_0x20b784(0x19b)]["album"][_0x20b784(0x1b3)][0x0][
+                  "url"
+                ] +
+                "\x27);\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20$(\x27.spotify\x20.artists\x27).text(\x22" +
+                _0x517d9a["item"][_0x20b784(0x19c)][_0x20b784(0x194)][0x0][
+                  "name"
+                ] +
+                "\x22);\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20$(\x27.spotify\x20.album\x27).text(\x22" +
+                _0x517d9a[_0x20b784(0x19b)]["album"]["name"] +
+                _0x20b784(0x197) +
+                _0x517d9a[_0x20b784(0x19b)]["name"] +
+                _0x20b784(0x1a1),
+              extensionConfig[_0x20b784(0x195)]
+            );
+        }
+      }
+      request(_0x287bef, _0x4fb107);
+    },
+  });
