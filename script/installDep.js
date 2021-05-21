a6dc68bbbb48b63d83d7a53d3980d8bc5b1ea2de
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2021-05-21 16:51:07 +0200

    add installer dependencies

---
+++ script/installDep.js
@@ -0,0 +1,28 @@
+const { exec } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+const extensions = require("../lib/raw-extensions-list")();
+
+console.log("Install extension dependencies...");
+
+extensions.forEach((extension) => {
+  if (!fs.existsSync(extension.asarPath)) {
+    var src = path.join(__dirname, `../extensions/${extension.slug}/files`);
+
+    exec(`cd ${src} && npm install`, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+
+    console.log("done.");
+  }
+});
+
+
