4b4cec41dd2ab0a25b363e0619e7a9ed4164f104
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2023-05-02 13:12:18 +0200

    REMOVE: rimraf

--- script/asarclean.js
+++ script/asarclean.js
@@ -1,14 +1,32 @@
 const fs = require("fs");
 const path = require("path");
 const extensions = require("../lib/raw-extensions-list")();
-const rimraf = require("rimraf").sync;
+
+function removeFileOrFolder(path) {
+  if (fs.existsSync(path)) {
+    const stats = fs.statSync(path);
+    if (stats.isFile()) {
+      fs.unlinkSync(path);
+    } else {
+      fs.readdirSync(path).forEach((file) => {
+        const curPath = `${path}/${file}`;
+        if (fs.lstatSync(curPath).isDirectory()) {
+          removeFileOrFolder(curPath);
+        } else {
+          fs.unlinkSync(curPath);
+        }
+      });
+      fs.rmdirSync(path);
+    }
+  }
+}
 
 console.log("Clean extension submission files dir...");
 
 extensions.forEach((extension) => {
   var src = path.join(__dirname, `../extensions/${extension.slug}/files`);
   if (fs.existsSync(src)) {
-    rimraf(src);
+    removeFileOrFolder(src);
 
     console.log("done.");
   }
